4a14b50425001d4798cc2c8aac22868321f39666
Sebastien <sebastiendumont1986@gmail.com>
2026-04-11 16:59:48 +0200

    More commands and screenshots

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,10 +15,38 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `search-docs` command */
   export type SearchDocs = ExtensionPreferences & {}
+  /** Preferences accessible in the `browse-endpoints` command */
+  export type BrowseEndpoints = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-hooks-filters` command */
+  export type SearchHooksFilters = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-error-codes` command */
+  export type SearchErrorCodes = ExtensionPreferences & {}
+  /** Preferences accessible in the `recent-docs` command */
+  export type RecentDocs = ExtensionPreferences & {}
+  /** Preferences accessible in the `quick-links` command */
+  export type QuickLinks = ExtensionPreferences & {}
+  /** Preferences accessible in the `whats-new` command */
+  export type WhatsNew = ExtensionPreferences & {}
+  /** Preferences accessible in the `menu-bar` command */
+  export type MenuBar = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
   /** Arguments passed to the `search-docs` command */
   export type SearchDocs = {}
+  /** Arguments passed to the `browse-endpoints` command */
+  export type BrowseEndpoints = {}
+  /** Arguments passed to the `search-hooks-filters` command */
+  export type SearchHooksFilters = {}
+  /** Arguments passed to the `search-error-codes` command */
+  export type SearchErrorCodes = {}
+  /** Arguments passed to the `recent-docs` command */
+  export type RecentDocs = {}
+  /** Arguments passed to the `quick-links` command */
+  export type QuickLinks = {}
+  /** Arguments passed to the `whats-new` command */
+  export type WhatsNew = {}
+  /** Arguments passed to the `menu-bar` command */
+  export type MenuBar = {}
 }
 
